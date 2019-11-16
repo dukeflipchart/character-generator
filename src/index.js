b7eaafb6a54136c071651121a135bcdee52d6a97
Csaba Schreiner <schreiner.csaba@gmail.com>
2019-11-17 00:11:12 +0100

    feat(*): add name lists, generate names

--- src/index.js
+++ src/index.js
@@ -81,50 +81,59 @@ class CharacterSheet extends React.Component {
         super(props);
 
         this.state = {
-            attributes: {
-                gender: {
-                    name: 'gender',
-                    value: this.generateSimpleAttributeValue(Asa.gender)
-                },
-                sexuality: {
-                    name: 'sexuality',
-                    value: this.generateSimpleAttributeValue(Asa.sexuality)
-                },
-                ancestry: {
-                    name: 'ancestry',
-                    value: this.generateSimpleAttributeValue(Asa.ancestry)
-                },
-                age: {
-                    name: 'age',
-                    value: this.generateSimpleAttributeValue(Asa.age)
-                }
+            gender: {
+                name: 'gender',
+                value: this.chooseValue(Asa.gender)
+            },
+            sexuality: {
+                name: 'sexuality',
+                value: this.chooseValue(Asa.sexuality)
+            },
+            ancestry: {
+                name: 'ancestry',
+                value: this.chooseValue(Asa.ancestry)
+            },
+            age: {
+                name: 'age',
+                value: this.chooseValue(Asa.age)
             }
+        }
+
+        this.state.givenName = {
+            name: 'givenName',
+            value: this.generateGivenName(Asa.givenName, this.state.ancestry.value, this.state.gender.value)
+        }
+
+        this.state.familyName = {
+            name: 'familyName',
+            value: this.generateFamilyName(Asa.familyName, this.state.ancestry.value, this.state.gender.value)
         }
     }
 
-    handleClick() {
-
+    handleClick(attribute) {
+        let previous = this.state[attribute].value;
+        //console.log(`previous state is: ${previous}`)
+        this.setState({
+            [attribute]: {
+                name: attribute,
+                value: this.chooseValue(Asa[attribute], previous)
+            }
+        });
     }
 
-    generateSimpleAttributeValue(options, oldValue) {
-        //console.log("oldValue: " + oldValue);
-        //console.log("values: " + this.props.values);
-
+    chooseValue(pool, oldValue) {
         // taking the last value out of the pool so we get a new one every time
-        let pool = options.filter(option => option !== oldValue);
-
-        //console.log("pool: " + pool);
-
+        pool = pool.filter(option => option.name !== oldValue);
         let sumWeights = 0;
         for (let index in pool) {
             //console.log("option.weight: " + pool[index].weight);
-            sumWeights += pool[index].weight;
+            sumWeights += pool[index].weight ? pool[index].weight : 1;
         }
         //console.log("sumWeights: " + sumWeights);
         let winner = Math.floor(Math.random() * sumWeights);
         //console.log("winner: " + winner);
         for (let index in pool) {
-            winner -= pool[index].weight;
+            winner -= pool[index].weight ? pool[index].weight : 1;
             //console.log("weight: " + pool[index].weight + ", winner is reduced to: " + winner)
             if (winner < 0) {
                 return pool[index].name;
@@ -132,13 +141,26 @@ class CharacterSheet extends React.Component {
         }
     }
 
+    generateGivenName(options, ancestry, gender, oldValue) {
+        if (gender === 'Cis Male' || gender === 'Trans Male') { gender = 'Male'; }
+        if (gender === 'Cis Female' || gender === 'Trans Female') { gender = 'Female'; }
+        if (gender === 'Genderfluid') { gender = (Math.random() >= 0.5) ? 'Male' : 'Female'; }
+        console.log(`name gender: ${gender}`);
+
+        return this.chooseValue(options[ancestry][gender], oldValue);
+    }
+
+    generateFamilyName(options, ancestry, oldValue) {
+
+        return this.chooseValue(options[ancestry], oldValue);
+    }
+
     render() {
         return (
             <div className="sheet">
                 {
-                    Object.values(this.state.attributes)
-                        .map(attribute => 
-                            <Attribute name={attribute.name} onClick={this.handleClick(attribute.name)} value={attribute.value} />
+                    Object.values(this.state).map(attribute => 
+                            <Attribute name={attribute.name} onClick={() => this.handleClick(attribute.name)} value={attribute.value} />
                         )
                 }
             </div>
